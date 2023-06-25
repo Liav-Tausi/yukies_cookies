418cb9a956b77e9bd3849a28ca66f53e1022d927
Liav <liavt242@gmail.com>
2023-06-25 16:46:02 +0300

    yukies_cookies initial structure

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+import express, {Express, Request, Response} from "express";
+import dotenv from "dotenv";
+import cors from "cors"
+import helmet from "helmet"
+import compression from "compression"
+
+dotenv.config();
+
+
+const app: Express = express();
+const port = process.env.PORT ;
+
+app.use(cors())
+app.use(helmet())
+app.use(compression())
+app.use(express.json())
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${process.env.PORT}`);
+});
